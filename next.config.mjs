c8dcbccda7a781f4ad3f071ee608cc15c57cf8e4
rnezzod <nnnn.tkrn@gmail.com>
2025-10-21 13:50:54 +0900

    ?

--- next.config.mjs
+++ next.config.mjs
@@ -3,15 +3,30 @@ import withPWA from 'next-pwa';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.microcms-assets.io'],
+    domains: ['images.microcms-assets.io', 'pbs.twimg.com'],
   },
 };
 
 const pwaConfig = withPWA({
   dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
   register: true,
   skipWaiting: true,
   runtimeCaching: [
+    {
+      urlPattern: /^https?:\/\/pbs\.twimg\.com\/.*/i,
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'twitter-images',
+        expiration: {
+          maxEntries: 100,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+        },
+        cacheableResponse: {
+          statuses: [0, 200],
+        },
+      },
+    },
     {
       urlPattern: /^https?:\/\/images\.microcms-assets\.io\/.*/i,
       handler: 'StaleWhileRevalidate',
