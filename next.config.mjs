128014039527806793aaf042c123dfc02936c3dc
rnezzod <nnnn.tkrn@gmail.com>
2025-10-21 13:38:41 +0900

    pwa mjs fix

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,22 @@ const pwaConfig = withPWA({
   dest: 'public',
   register: true,
   skipWaiting: true,
+  runtimeCaching: [
+    {
+      urlPattern: /^https?:\/\/images\.microcms-assets\.io\/.*/i,
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'microcms-images',
+        expiration: {
+          maxEntries: 100,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+        },
+        cacheableResponse: {
+          statuses: [0, 200],
+        },
+      },
+    },
+  ],
 });
 
 const configWithPWA = pwaConfig(nextConfig);
